3ea640eede92c1f48ff0573b82df3db6bb6a1b0c
bonni <bonni@PCBNR>
2019-01-10 15:25:19 +0100

    update

---
+++ app/src/main/java/com/example/bonni/ben/CreateAccount/CreateAccountActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bonni.ben.CreateAccount
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bonni.ben.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+    }
+}
---
+++ app/src/main/java/com/example/bonni/ben/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bonni.ben
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
