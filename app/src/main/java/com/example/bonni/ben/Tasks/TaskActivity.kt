0144f712a1e91da68c1e9fd945a80dea44c85a9c
Anne <anne.lagarde@hetic.net>
2019-01-10 16:55:51 +0100

    inte page list

---
+++ app/src/main/java/com/example/bonni/ben/Tasks/TaskActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bonni.ben.Tasks
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bonni.ben.R
+
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+    }
+}
